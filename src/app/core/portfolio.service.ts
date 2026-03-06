cd81238dbacd9ca7b1edfdb0e0998b8e1e172d7a
Javier Martínez Valiente <javier_martinez92@hotmail.com>
2026-03-06 22:31:59 +0100

    fix: use relative path for data.json to work with gh-pages base-href

--- src/app/core/portfolio.service.ts
+++ src/app/core/portfolio.service.ts
@@ -9,9 +9,9 @@ import { IPortfolioData } from './portfolio.interfaces';
 export class PortfolioService {
     private readonly http = inject(HttpClient);
 
-    // Convierte el Observable del HttpClient directo a un Signal reactivo.
     // En Angular 20 (Vite builder), el contenido de /public se sirve desde la raíz ('/data.json').
+    // Al usar Github Pages con subdirectorio, conviene poner la ruta relativa 'data.json' para que coja el base-href.
     public readonly portfolioData: Signal<IPortfolioData | undefined> = toSignal(
-        this.http.get<IPortfolioData>('/data.json')
+        this.http.get<IPortfolioData>('data.json')
     );
 }
