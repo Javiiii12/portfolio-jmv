48331e85afa3e85f3b7e5fc65e4cbea0ca28413e
Javier Martínez Valiente <javier_martinez92@hotmail.com>
2026-03-07 17:17:52 +0100

    fix: oswe verification link and typo cleanup

--- src/app/core/portfolio.interfaces.ts
+++ src/app/core/portfolio.interfaces.ts
@@ -64,7 +64,7 @@ export interface ICertDetail {
     reviewBest: string;
     reviewWorst: string;
     verdict: string;
-    verifyLink?: string;
+    verificationUrl?: string;
 }
 
 export interface IPortfolioData {
